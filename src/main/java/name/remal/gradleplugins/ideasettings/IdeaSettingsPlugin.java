c546af7b595274202559d5166179446fefaab134
Semyon Levin <231188+remal@users.noreply.github.com>
2022-08-04 20:16:14 -0700

    Update IdeaSettingsPlugin.java

--- src/main/java/name/remal/gradleplugins/ideasettings/IdeaSettingsPlugin.java
+++ src/main/java/name/remal/gradleplugins/ideasettings/IdeaSettingsPlugin.java
@@ -72,7 +72,8 @@ public class IdeaSettingsPlugin implements Plugin<Project> {
             if (repositoryRootPath != null && !topLevelDirPath.equals(repositoryRootPath)) {
                 logger.warn(
                     "Skipping logic of {}, as top level dir ({}) differs from Git repository root ({}). You can "
-                        + "explicitly enable the logic by adding `ideaSettings.explicitlyEnabled = true` to the build script.",
+                        + "explicitly enable the logic by adding `ideaSettings.explicitlyEnabled = true` "
+                        + "to the build script.",
                     new PluginDescription(IdeaSettingsPlugin.class),
                     topLevelDirPath,
                     repositoryRootPath
